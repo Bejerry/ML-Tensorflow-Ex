9dc416cb330f1aad0a8d7f3432f1b7f650297d55
Bejerry <bejerry@126.com>
2017-02-12 23:11:25 +0800

    Update BP.py
    Add running result.

--- BP.py
+++ BP.py
@@ -40,6 +40,20 @@ for step in range(200):
 w = sess.run(Weights)
 b = sess.run(Biases)
 
-print("Result: Weight ----- Biases")
+print("Result is: Weight ----- Biases")
 print(w, b)
+Running-Result：
+I c:\tf_jenkins\home\workspace\release-win\device\gpu\os\windows\tensorflow\core\common_runtime\gpu\gpu_device.cc:975] Creating TensorFlow device (/gpu:0) -> (device: 0, name: GeForce 940MX, pci bus id: 0000:01:00.0)
+0 [ 0.15538068] [ 0.36024028]
+20 [ 0.10616058] [ 0.29687488]
+40 [ 0.10192242] [ 0.29902482]
+60 [ 0.10059989] [ 0.2996957]
+80 [ 0.10018721] [ 0.29990506]
+100 [ 0.10005846] [ 0.29997036]
+120 [ 0.10001825] [ 0.29999074]
+140 [ 0.10000572] [ 0.29999712]
+160 [ 0.10000179] [ 0.29999909]
+180 [ 0.10000055] [ 0.29999971]
 
+Result is: Weight ----- Biases
+       [ 0.10000017] [ 0.29999992]
